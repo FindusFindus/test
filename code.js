8ad7299649e669d08dbae226e6462502b50f4635
Hanna Nordberg <hanna_nordberg@hotmail.com>
2018-03-14 11:45:33 +0100

    hello oops

---
+++ code.js
@@ -0,0 +1,48 @@
+var $ = require("jQuery");
+
+function renderMovie(movieData) {
+    document.getElementById("movietitle").innerText = movieData.Title;
+    document.getElementById("moviedescription").innerText = movieData.Description;
+    document.getElementById("poster").setAttribute("src", movieData.Poster);
+    //  document.getElementById("actors").innerHTML = movieData.Actors;
+    let actorHtml = "";
+    movieData.Actors.forEach(actor => {
+        actorHtml = actorHtml + "<li>" + actor + "</li>";
+        console.log(actorHtml)
+    });
+    document.getElementById("actors").innerHTML = actorHtml;
+} 
+let movieData = require("./data")   
+renderMovie(movieData);
+
+function changeStarRating(grade) {
+    let currentlyFilledStars = document.querySelectorAll(".filledStar");
+    currentlyFilledStars.forEach(element => {
+        element.className = "emptyStar";
+    });
+    
+    for (let index = 1; index <= grade; index++) {
+        document.getElementById("star" + index).className = "filledStar";
+    };
+}
+
+for (let index = 1; index <= 5; index++) {
+    document.getElementById("star" + index).addEventListener("click", () => {
+        changeStarRating(index);
+    })      
+};
+
+// function hover(grade) {
+
+// for (let index = 1; index <= 5; index++) {
+//     document.getElementById("star" + index).addEventListener("mouseover", () => {
+//         hover(index);
+//     })      
+// };
+
+// for (let index = 1; index <= 5; index++) {
+//     document.getElementById("star" + index).addEventListener("mouseout", () => {
+//         hover(0);
+//     })      
+// };
+
---
+++ data.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Title: "Inception",
+    Description: "A very meta movie about dreams within dreams. And then there was a whole bunch of shooting. not realy sure how that fit into the picture",
+    Actors: ["Leonardo DiCaprio","Ellen Page","Tom Hardy"],
+    Poster: "http://dizw242ufxqut.cloudfront.net/images/product/movie/dvd/image0/inception-7528624-frntl.jpg",
+}
